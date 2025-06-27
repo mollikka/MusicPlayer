ba53698cdf7649b989c89d6e16626b03350a3113
Lauri Tervonen <lauri.tervonen@reaktor.com>
2025-06-27 16:57:17 +0300

    Package css style inside the compiled js

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import css from "./index.css?inline";
 import { MusicApp } from "./components/MusicApp.tsx";
 
 const rootElement = document.getElementById("root")!;
 const playlistUrl = rootElement.dataset.playlistUrl!;
+
+const style = document.createElement("style");
+style.textContent = css;
+document.head.appendChild(style);
 
 createRoot(rootElement).render(
   <StrictMode>
